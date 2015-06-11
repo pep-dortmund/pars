9e6491b86e74bcc5185ffa10db2eeb6816e9ff47
Kevin Heinicke <kevin@kehei.de>
2015-06-11 11:25:37 +0200

    checking the form, add alerts

--- static/script.js
+++ static/script.js
@@ -1,9 +1,37 @@
-$(document).ready(function(){
-    $('button#submit').click(function(){
-        event.preventDefault();
-        var data = $("#form").serializeArray();
-        $.post("post/", data, function(){
-            console.log("success");
-        });
+function checkValues(data){
+    var required = 0;
+    $(data).each(function(index, value){
+        if(value.value == ""){
+            required = required + 1;
+        }
     });
+    if(required == 0){
+        return true;
+    }
+    return false;
+}
+
+function highlightMissing(){
+    $("input.required").each(function(index, input){
+        console.log($(input).val());
+        if(!$(input).val()){
+            $(input).parent().parent().addClass("has-error");
+        } else {
+            $(input).parent().parent().removeClass("has-error");
+        }
+    });
+}
+
+$(document).ready(function(){
+    $("#form").submit(function(){
+        event.preventDefault();
+        var data = $(this).serializeArray();
+        highlightMissing();
+        if(checkValues(data)) {
+            $.post($(this).attr("action"), data, function(){
+                console.log("success");
+                $("#alert-added-successfully").removeClass("hidden");
+            });
+        }
+    })
 });
