cec35c1c995977708453af6a62adba7f6d6e3451
Kevin Heinicke <kevin@kehei.de>
2015-07-21 14:13:17 +0200

    including KaTeX for testing
    i would like to add inline-math functionality in latex-style with
    $$-><span with latex></span>

--- static/js/script.js
+++ static/js/script.js
@@ -5,19 +5,31 @@
         return {
             restrict: 'E',
             templateUrl: 'templates/main-form.html',
-            controller: function(){
+            controller: ['$sce', function($sce){
                 this.mailDomain = '@tu-dortmund.de';
                 this.degrees = {
                     'ba': {'id': 'ba', 'name': 'Bachelor'},
                     'ma': {'id': 'ma', 'name': 'Master'},
                     'dr': {'id': 'dr', 'name': 'Doktor'}
                 }
+                this.title = "";
 
                 var participant = this;
+
+                this.updateTex = function(){
+                    try {
+                        participant.title = participant.title || "";
+                        participant.tex = $sce.trustAsHtml(
+                            "Machbarkeitsstudie " + katex.renderToString(participant.title) + " blabla"
+                        );
+                    }
+                    catch(ParseError){ }
+                }
+
                 this.save = function(){
                     console.log("Saving Participant. " + participant.firstname);
                 };
-            },
+            }],
             controllerAs: 'participant'
         }
     });
