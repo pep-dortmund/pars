61a8fd0e489d532db1c85f7ca4a9673395d7f06e
Kevin Heinicke <kevin@kehei.de>
2015-07-22 21:11:22 +0200

    Live LaTeX <3 KaTeX is awesome

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,21 @@
+function seperateTeX(string){
+    var stringArray = [];
+    var index = 0;
+    while(index > -1){
+        index = string.indexOf("$") > -1 ? string.indexOf("$") : string.length;
+        string.substring(0, index) ?
+            stringArray.push(string.substring(0, index)) : undefined;
+        string = string.substring(index + 1, string.length);
+        index = string.indexOf("$");
+        if(index > -1){
+            string.substring(0, index) ?
+                stringArray.push("$" + string.substring(0, index)) : undefined;
+            string = string.substring(index+1, string.length);
+        }
+    }
+    return stringArray;
+}
+
 (function(){
     var app = angular.module("app", []);
 
@@ -19,9 +37,19 @@
                 this.updateTex = function(){
                     try {
                         participant.title = participant.title || "";
-                        participant.tex = $sce.trustAsHtml(
-                            "Machbarkeitsstudie " + katex.renderToString(participant.title) + " blabla"
-                        );
+                        var stringArray = seperateTeX(participant.title);
+                        var completeString = "";
+                        for(var i=0; i<stringArray.length; i++){
+                            var string = stringArray[i];
+                            if(string.substring(0, 1) == "$"){
+                                completeString += katex.renderToString(
+                                    string.substring(1, string.length)
+                                );
+                            } else {
+                                completeString += string;
+                            }
+                        }
+                        participant.tex = $sce.trustAsHtml(completeString);
                     }
                     catch(ParseError){ }
                 }
