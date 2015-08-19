a68ea77c1eb2e9ecb6f6ce16a030e506f9fa5056
Kevin Heinicke <kevin@kehei.de>
2015-08-19 20:50:33 +0200

    using bootstrap 4 alpha

--- static/js/script.js
+++ static/js/script.js
@@ -23,70 +23,121 @@ var NameInput = React.createClass({
         }
     },
     handleChange: function() {
-        this.firstname = this.refs.firstname.getDOMNode().value;
-        this.lastname = this.refs.lastname.getDOMNode().value;
-        this.props.onUserInput(this.firstname, this.lastname);
+        this.setState({
+            firstname: this.refs.firstname.getDOMNode().value,
+            lastname: this.refs.lastname.getDOMNode().value
+        });
+        this.props.onUserInput(this.state.firstname, this.state.lastname);
+        if(this.state.error){
+            this.check();
+        }
     },
     check: function() {
-        if(!this.firstname || !this.lastname){
-            this.setState({error: true});
-        } else {
-            this.setState({error: false});
-        }
+        this.setState({error: (!this.state.firstname || !this.state.lastname)});
     },
     render: function(){
         var classes = React.addons.classSet({
             'form-group': true,
             'has-error': this.state.error
         });
+        var firstnameClasses = React.addons.classSet({
+            'form-control': true,
+            'form-control-error': this.state.error && !this.state.firstname
+        });
+        var lastnameClasses = React.addons.classSet({
+            'form-control': true,
+            'form-control-error': this.state.error && !this.state.lastname,
+        });
         var hint = !this.state.error ? '' : (
-            <span className="help-block col-sm-offset-4 col-sm-8">
+            <span className="help-block">
                 Bitte trage Vor- und Nachnamen ein.
                 So wirst du bei der Absolventenfeier aufgerufen.
             </span>);
         return (
-            <div className={classes}>
-                <label className="col-sm-4 control-label">Name</label>
-                <div className="col-sm-4">
-                    <input
-                        className="form-control"
-                        placeholder="Max"
-                        ref="firstname"
-                        onChange={this.handleChange}/>
-                </div>
-                <div className="col-sm-4">
-                    <input
-                        className="form-control"
-                        placeholder="Mustermann"
-                        ref="lastname"
-                        onChange={this.handleChange}/>
+            <fieldset className={classes}>
+                <label className="control-label">Name</label>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <input
+                            className={firstnameClasses}
+                            placeholder="Max"
+                            ref="firstname"
+                            onChange={this.handleChange}/>
+                    </div>
+                    <div className="col-sm-6">
+                        <input
+                            className={lastnameClasses}
+                            placeholder="Mustermann"
+                            ref="lastname"
+                            onChange={this.handleChange}/>
+                    </div>
                 </div>
                 {hint}
-            </div>
+            </fieldset>
         );
     }
 });
+
+var EmailInput = React.createClass({
+    getInitialState: function(){
+        return {
+            error: false
+        }
+    },
+    check: function(){
+        this.setState({error: (!this.email || !this.email.match(/^\w+\.\w+$/i))});
+    },
+    handleChange: function(){
+        this.email = this.refs.email.getDOMNode().value;
+        this.props.onUserInput(this.email);
+        if(this.state.error){
+            this.check();
+        }
+    },
+    render: function(){
+        var hint = !this.state.error ? '' : (
+                <span className="help-block">
+                    Bla
+                </span>
+            );
+        var classes = React.addons.classSet({
+            'form-group': true,
+            'has-error': this.state.error
+        });
+        return (
+            <fieldset className={classes}>
+                <label className="control-label">Unimail-Adresse</label>
+                <div className="input-group">
+                    <input
+                        className="form-control"
+                        placeholder="max.mustermann"
+                        ref="email"
+                        onChange={this.handleChange}/>
+                    <div className="input-group-addon">@tu-dortmund.de</div>
+                </div>
+                {hint}
+            </fieldset>
+        );
+    }
+})
 
 var ParticipantForm = React.createClass({
     handleSubmit: function(e) {
         e.preventDefault();
         this.refs.name.check();
+        this.refs.email.check();
         return;
     },
     nameInput: function(firstname, lastname){
-        this.props.firstname = firstname;
-        this.props.lastname = lastname;
-        console.log(this.props);
+    },
+    emailInput: function(email){
     },
     render: function(){
         return (
-            <form className="form-horizontal" onSubmit={this.handleSubmit}>
+            <form onSubmit={this.handleSubmit}>
                 <NameInput ref="name" onUserInput={this.nameInput} />
-                <div className="form-group">
-                    <div className="col-sm-offset-4 col-sm-8">
-                        <input type="submit" className="btn btn-default" value="Post" />
-                    </div>
-                </div>
+                <EmailInput ref="email" onUserInput={this.emailInput} />
+                <input type="submit" className="btn btn-secondary" value="Post" />
             </form>
         );
     }
