f34e5f2c130b2e34f23b76ebca76e4e8aa19195f
Kevin Heinicke <kevin@kehei.de>
2015-10-21 21:14:40 +0200

    add deployment config dummy stuff

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 class Config(object):
     MAXIMUM_GUESTS = 10
     MAIL_PORT = 587
+    MAIL_ADDRESS = ''
+    MAIL_SERVER = ''
+    MAIL_LOGIN = ''
+    MAIL_PASSWORD = ''
     DEBUG = False
     TESTING = False
 
